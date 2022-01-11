25c58e6d6b1204d60abfe75f8d58bc75ac6f4f91
Ivan Lombardi Borgia <ivan.lombardiborgia@gmail.com>
2022-01-11 11:14:06 +0100

    returns Fizz for multiples of 3

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,8 @@
 public class FizzBuzz {
 
-    public String say(int number) {
+    public static String say(int number) {
+        if (number % 3 == 0)
+            return "Fizz";
         return String.valueOf(number);
     }
 }
