6a40de92ec9c481a22e9aef6861fe278e20e61ae
Tommaso Torti <torti.tommaso@gmail.com>
2022-05-23 17:43:08 +0200

    xxx

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,15 @@
 public class FizzBuzz {
     public String say(int number) {
+        if(isMultipleOf(number, 3)) {
+          return "Fizz";
+        }
+        if(isMultipleOf(number, 5)) {
+            return "Buzz";
+        }
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf(int input, int number) {
+        return input % number == 0;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,13 +3,24 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class FizzBuzzTest {
+    FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
     void plain_numbers_case() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
         assertThat(fizzBuzz.say(1)).isEqualTo("1");
         assertThat(fizzBuzz.say(2)).isEqualTo("2");
+    }
+
+    @Test
+    void multiple_of_3(){
+        assertThat(fizzBuzz.say(3)).isEqualTo("Fizz");
+        assertThat(fizzBuzz.say(6)).isEqualTo("Fizz");
+    }
+
+    @Test
+    void multiple_of_5(){
+        assertThat(fizzBuzz.say(5)).isEqualTo("Buzz");
+        assertThat(fizzBuzz.say(10)).isEqualTo("Buzz");
     }
 
 }
