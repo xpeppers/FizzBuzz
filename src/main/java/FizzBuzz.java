f9db1fd4f06ac72597ec05931eeb5d1e32a4aa23
fabuloso <dalfovo.alessandro@gmail.com>
2022-05-25 10:17:25 +0200

    Add multiple of 3 case

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,10 @@
 public class FizzBuzz {
     public String say(int number) {
+        if (isMultipleOf3(number)) return "Fizz";
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -3,11 +3,27 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class FizzBuzzTest {
+    private final FizzBuzz fizzBuzz = new FizzBuzz();
 
     @Test
     void firstNumber() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String output = fizzBuzz.say(1);
+
         assertThat(output).isEqualTo("1");
     }
+    @Test
+    void secondNumber() {
+        String output = fizzBuzz.say(2);
+
+        assertThat(output).isEqualTo("2");
+    }
+
+    @Test
+    void numberIsMultipleOf3() {
+        String output = fizzBuzz.say(3);
+
+        assertThat(output).isEqualTo("Fizz");
+    }
+
+
 }
